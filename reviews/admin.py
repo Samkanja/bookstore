c574cdbaf474aab2b664ec4a2d0208c59936de04
Samkanja <samuelkanja4@gmail.com>
2023-03-27 17:48:11 -0700

    register models to admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Review,Book,BookContributor,Contributor,Publisher
+
+
+admin.site.register(Publisher)
+admin.site.register(Contributor)
+admin.site.register(Book)
+admin.site.register(BookContributor)
+admin.site.register(Review)
